af74a401623155a2d49ef2c5912b6513fa72938f
Malte Ahlers <malteahlers@Maltes-MBP.fritz.box>
2023-01-31 15:13:03 +0100

    deploy script

--- buildimage.sh
+++ buildimage.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# getting username
+user=$(cat dockeruser.txt)
+
 # docker login
-cat dockerpassword.txt | docker login --username franclin1 --password-stdin
+cat dockerpassword.txt | docker login --username $user --password-stdin
 
 # build node-image
 docker buildx build --platform=linux/amd64 -t node-app -f ./node-app/Dockerfile ./node-app/
-docker tag node-app franclin1/demo-app:node-1.0
-docker push franclin1/demo-app:node-1.0
+docker tag node-app $user/demo-app:node-1.0
+docker push $user/demo-app:node-1.0
 
 # build java-image
 cd java-app-efk
 ./gradlew build
 docker buildx build --platform=linux/amd64 -t java-app -f Dockerfile .
-docker tag java-app franclin1/demo-app:java-1.0
-docker push franclin1/demo-app:java-1.0
+docker tag java-app $user/demo-app:java-1.0
+docker push $user/demo-app:java-1.0
